b0d7985ecc8ff4d1932775e6b3d38ee6c3bda249
SuperPertti <xzero.muumimaailma@gmail.com>
2013-03-19 18:46:53 +0200

    nimi muutettu

--- LPKJasenrekisteri/test/SyntymaaikaTest.java
+++ LPKJasenrekisteri/test/SyntymaaikaTest.java
@@ -15,9 +15,9 @@ import static org.junit.Assert.*;
  *
  * @author Pertti
  */
-public class LPKJasenrekisteriTest {
+public class SyntymaaikaTest {
     
-    public LPKJasenrekisteriTest() {
+    public SyntymaaikaTest() {
     }
     
     @BeforeClass
